0dd78583acb94976ccd765cd074bf06ec33ad60e
gskapoor <kapoorgobind@gmail.com>
2024-12-18 01:30:11 -0800

    Basic AST definitions

--- parser.py
+++ parser.py
@@ -1,3 +1,44 @@
 
+
+class ASTNode:
+    # Base Node Class
+    pass
+
+class ProgramNode(ASTNode):
+    def __init__(self, function):
+        self.function = function
+
+    def __repr__(self):
+        return (f"ProgramNode({repr(self.functionDef)})")
+
+class FunctionNode(ASTNode):
+    def __init__(self, identifier, statement):
+        self.identifier = identifier
+        self.statement = statement
+
+    def __repr__(self):
+        return (f"FunctionNode({repr(self.identifier), repr(self.statement)})")
+
+class ReturnNode(ASTNode):
+    def __init__(self, expression):
+        self.expression = expresion
+
+    def __repr__(self):
+        return (f"ReturnNode({repr(self.expression)})")
+
+class Expression(ASTNode):
+    def __init__(self, constant):
+        self.constant = constant
+
+    def __repr__(self):
+        return (f"ReturnNode({repr(self.constant)})")
+
+class Constant(ASTNode):
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return repr(self.value)
+
 def parse(input):
-    return []
+    return Constant(1)
--- pyCmp.py
+++ pyCmp.py
@@ -5,11 +5,18 @@ import lexer
 import parser
 import asmgen
 
-def compile(fileInName, fileOutName):
+def compile(fileInName, fileOutName, mode):
     with open(fileInName, "r") as fileIn:
         lexPut = lexer.lex(fileIn.read())
         parsePut = parser.parse(lexPut)
         asm = asmgen.asmgenerate(parsePut)
+        # for now, use this dummy value:
+        asm =  """
+         .globl main
+        main:
+         movl $2, %eax
+         ret
+        """
         with open(fileOutName, "w") as fileOut:
             fileOut.write(asm)
     return True

5793e1d31a1d4a6e8c1ddbbd84addacbde0dbb86
gskapoor <kapoorgobind@gmail.com>
2024-12-19 22:51:12 -0800

    linted

--- pyCC/pyCC.py
+++ pyCC/pyCC.py
@@ -3,16 +3,20 @@
 
 import sys
 import subprocess
-from pyCmp import pyCmp
+from .pyCmp import pyCmp
 
 def main():
+    """
+    _summary_ Main Function for Compiler 
+    """
 
     args = sys.argv
     mode = 3
     if (len(args) < 2 or len(args) > 3):
         print(f"Usage: {args[0]} <C file> {{--lex|--parse|--codegen}}", file=sys.stderr)
-        return 1;
-    elif (len(args) == 3):
+        return 1
+
+    if len(args) == 3:
         if args[2] == "--lex":
             mode = 0
         elif args[2] == "--parse":
@@ -21,8 +25,7 @@ def main():
             mode = 2
         else:
             print(f"Usage: {args[0]} <C file> {{--lex|--parse|--codegen}}", file=sys.stderr)
-            return 1;
-
+            return 1
 
     file_name = args[1]
     if file_name[-2:] != ".c":
@@ -33,7 +36,10 @@ def main():
 
     result = subprocess.run(["gcc", "-E", "-P", file_name, "-o", f"{file_prefix}.i"],
                             capture_output=True,
-                            text=True)
+                            text=True,
+                            check=True
+                            )
+
     if result.returncode != 0:
         print("Err: Unable to PreProcess",
               file=sys.stderr)
@@ -43,25 +49,27 @@ def main():
         return 1
     ## Compile time : )
     pyCmp.compile(f"{file_prefix}.i", f"{file_prefix}.s", mode)
-    subprocess.run(["rm", f"{file_prefix}.i" ])
+    subprocess.run(["rm", f"{file_prefix}.i" ], check=True)
 
     ## Assembling time : )
     result = subprocess.run(["gcc", f"{file_prefix}.s", "-o", file_prefix],
                             capture_output=True,
-                            text=True)
+                            text=True,
+                            check = True
+                            )
     if result.returncode != 0:
         print("Err: Unable to PreProcess",
               file=sys.stderr)
         print(result.stderr,
               file=sys.stderr,
               end="")
-        return 1;
+        return 1
 
-    subprocess.run(["rm", f"{file_prefix}.s" ])
+    subprocess.run(["rm", f"{file_prefix}.s" ], check=True)
 
     return 0
 
 if __name__ == "__main__":
-    exit_code = main()
-    sys.exit(exit_code)
+    exitCode = main()
+    sys.exit(exitCode)
     

6cb261b58393e44f0b97d28ab5548e55a247358c
gskapoor <kapoorgobind@gmail.com>
2024-12-23 03:36:11 -0800

    updated lexing for chapter 3

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -3,23 +3,39 @@ import pyCC.pyCmp.lexer as lexer
 
 
 class TestLexer(unittest.TestCase):
-    def test_int_parsing(self):
+    def test_int_lexing(self):
         result = lexer.lex("int")
         self.assertEqual(result, [(lexer.TokenType.INT, "int")])
 
-    def test_neg_parsing(self):
+    def test_neg_lexing(self):
         result = lexer.lex("-")
         self.assertEqual(result, [(lexer.TokenType.NEGATE, "-")])
 
-    def test_bitflip_parsing(self):
+    def test_bitflip_lexing(self):
         result = lexer.lex("~")
         self.assertEqual(result, [(lexer.TokenType.BITFLIP, "~")])
 
-    def test_decr_parsing(self):
+    def test_decr_lexing(self):
         ## For now, we want an "error"
         with self.assertRaises(NotImplementedError):
             lexer.lex("--")
         # self.assertEqual(result, [(lexer.TokenType.NEGATE, "-")])
+
+    def test_plus_lexing(self):
+        result = lexer.lex("+")
+        self.assertEqual(result, [(lexer.TokenType.PLUS, "+")])
+
+    def test_ast_lexing(self):
+        result = lexer.lex("*")
+        self.assertEqual(result, [(lexer.TokenType.ASTERISK, "*")])
+
+    def test_fslash_parsing(self):
+        result = lexer.lex("/")
+        self.assertEqual(result, [(lexer.TokenType.FSLASH, "/")])
+
+    def test_mod_parsing(self):
+        result = lexer.lex("%")
+        self.assertEqual(result, [(lexer.TokenType.MODULUS, "%")])
 
 
 if __name__ == "__main__":
